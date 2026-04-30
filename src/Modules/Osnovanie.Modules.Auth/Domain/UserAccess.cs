b44f999b694461cb08b9abd3db94a1b2b1cd905c
michil <fitim1501@gmail.com>
2026-04-30 13:24:44 +0900

    test

---
+++ src/Modules/Osnovanie.Modules.Auth/Domain/UserAccess.cs
@@ -0,0 +1,26 @@
+﻿using CSharpFunctionalExtensions;
+using Osnovanie.Shared;
+
+namespace Osnovanie.Modules.Auth.Domain;
+
+public sealed class UserAccess
+{
+    public Guid Id { get; private set; }
+
+    public Guid UserId { get; private set; }
+
+    public string ApplicationCode { get; private set; } = null!; // VDele, VLavke, AdminPanel
+
+    public string RoleCode { get; private set; } = null!; // Customer, Specialist, Admin...
+
+    public DateTime CreatedAt { get; private set; }
+
+    public static Result<UserAccess, Error> Create(
+        Guid userId,
+        string applicationCode,
+        string roleCode
+        )
+    {
+        if (us)
+    }
+}
